f4811da0c96533d08e676a54f19d889d25696afd
Popsam806 <popoolasamuel806@gmail.com>
2024-02-20 18:27:53 +0100

    Animate spline

--- src/App.js
+++ src/App.js
@@ -7,6 +7,32 @@ import Slick from './Components/Slick';
 import "./App.css"
 import Spline from '@splinetool/react-spline';
 
+let i = 0.4
+function zoom_scroll(spline) {
+ 
+    spline.setZoom(5)
+
+}
+
+// const startSpin = (splineRef, speed=1) => {
+//   const spline = splineRef.current;
+//   const objectToRotate = spline.root() // Replace with your object name
+
+//   if (!objectToRotate) {
+//     console.error('Object not found:', 'YourObjectName');
+//     return;
+//   }
+
+//   function spin() {
+//     const rotationSpeed = 360 * speed; // Degrees per second
+
+//     objectToRotate.transform.rotation.y += rotationSpeed * 0.001; // Adjust for time unit (1ms)
+
+//     requestAnimationFrame(spin);
+//   }
+
+//   spin();
+// };
 
 
 
@@ -17,7 +43,8 @@ function App() {
       <Nav />
       <Spline scene="https://prod.spline.design/SyN0pZF-50T8eGUK/scene.splinecode" style={{
         position: "absolute",
-      }} />
+        
+      }} onLoad={zoom_scroll} />
       <Header />
       <Services />
       <OurWork />
